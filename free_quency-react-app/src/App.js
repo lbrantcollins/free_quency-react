2c6deb92735733cca8457331555eef091b350cd7
lbrantcollins <lbrantcollins@gmail.com>
2019-08-20 15:44:59 -0500

    register form created

--- free_quency-react-app/src/App.js
+++ free_quency-react-app/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import './App.css';
+import Register from './Register'; 
+  
+class Hello extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      username: '',
+      password: '',
+      email: '',
+      about: '',
+      image: {},
+      loading: true
+    }
+  }
+
+  register = async (data) => {
+     try {
+
+      const registerResponse = await fetch('http://localhost:8000/user/register', {
+        method: 'POST',
+        credentials: 'include',// on every request we have to send the cookie
+        body: data,
+        headers: {
+          'enctype': 'multipart/form-data'
+        }
+      })
+
+      const parsedResponse = await registerResponse.json();
+
+      console.log(parsedResponse)
+
+      this.setState({
+        ...parsedResponse.data,
+        loading: false
+      })
+      return parsedResponse;
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  // what should the component render
+  render () {
+    // Make sure to return some UI
+    return (
+      <div>
+      
+      <Register register={this.register}/>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+
+export default Hello
