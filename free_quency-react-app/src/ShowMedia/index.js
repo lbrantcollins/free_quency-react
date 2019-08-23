d1b435ce9a0eb6064f96ec7e9b6e046b0934dda6
Dan <dnshaw12@gmail.com>
2019-08-23 10:49:55 -0500

    media show page done

--- free_quency-react-app/src/ShowMedia/index.js
+++ free_quency-react-app/src/ShowMedia/index.js
@@ -4,30 +4,53 @@ import { Link } from 'react-router-dom';
 
 import EditMedia from '../EditMedia'
 
-class FeaturedMedia extends Component {
+class ShowMedia extends Component {
 	constructor(props){
 		super(props)
 
 		this.state = {
-			id: this.props.media.id,
-			title: this.props.media.title,
-			url: this.props.media.url,
-			description: this.props.media.description,
-			full_html: this.props.media.full_html,
-			comments: this.props.media.comments,
-			favorites: this.props.media.favorites,
-			user_id: this.props.media.user_id,
+			id: null,
+			title: null,
+			url: null,
+			description: null,
+			full_html: null,
+			comments: [],
+			favorites: [],
+			user_id: null,
 			editActive: false
 
 		}
 	}
 
 
-	componentDidMount(){
-		console.log(this.props.media);
-		// this.setState({
-		// 	...this.props.media
-		// })
+	async componentDidMount(){
+
+		const { handle } = this.props.match.params
+
+		try {
+
+	      const mediaResponse = await fetch('http://localhost:8000/media/' + this.props.match.params.id, {
+	        method: 'GET',
+	        credentials: 'include',// on every request we have to send the cookie
+	        headers: {
+	          'enctype': 'multipart/form-data'
+	        }
+	      })
+
+	      const parsedResponse = await mediaResponse.json();
+
+	      console.log(parsedResponse.data);
+
+	      this.setState({
+	        ...parsedResponse.data
+	      })
+
+	      console.log(this.state.user);
+	    } catch (err) {
+	      console.log(err)
+	    }
+
+
 
 		console.log(this.state);
 	}
@@ -53,7 +76,7 @@ class FeaturedMedia extends Component {
 
 	      const parsedResponse = await updateMediaResponse.json();
 
-	      console.log(parsedResponse, "parsedResponse in FeaturedMedia");
+	      console.log(parsedResponse, "parsedResponse in ShowMedia");
 
 	      if (parsedResponse.status.code === 201) {
 	      	this.setState({
@@ -91,7 +114,11 @@ class FeaturedMedia extends Component {
 				<Divider />
 				<Container textAlign='left'>
 					<a href={this.state.url}><Header as='h2'>{this.state.title}</Header></a>
-					<div>By: <Link to={'/user/' + this.state.user_id.id}>{this.state.user_id.username}</Link></div>
+					<div>By: 
+						<Link to={'/user/' + (this.state.user_id ? this.state.user_id.id : null)}>
+							{this.state.user_id ? this.state.user_id.username : null}
+						</Link>
+					</div>
 					<div>Favorites: {this.state.favorites.length}</div>
 					<Link onClick={this.toggleEdit}>Edit</Link>
 					{this.state.editActive ? <EditMedia media={this.state} handleEdit={this.handleEdit}/> : null}
@@ -108,4 +135,4 @@ class FeaturedMedia extends Component {
 	}
 }
 
-export default FeaturedMedia
+export default ShowMedia
